b63b50fe1185ed19a49a659f6df089971fa30cb0
Zachary K Edgell <zachary@accrue.tech>
2021-12-25 01:58:52 -0500

    Added results and updated test.

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,10 @@ mod tests {
     use near_sdk::{testing_env, VMContext};
     use crate::structs::project_management::ProjectManagement;
 
-    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+    fn get_context(input: Vec<u8>, is_view: bool, signer: String) -> VMContext {
         VMContext {
             current_account_id: "alice.testnet".to_string(),
-            signer_account_id: "robert.testnet".to_string(),
+            signer_account_id: signer,
             signer_account_pk: vec![0, 1, 2],
             predecessor_account_id: "jane.testnet".to_string(),
             input,
@@ -38,22 +38,24 @@ mod tests {
     // This test should insert a new project with no errors
     fn test_get_all_projects() {
         // set up the mock context into the testing environment
-        let context = get_context(vec![], false);
+        let context = get_context(vec![], false, "alice.testnet".to_string());
         testing_env!(context.clone());
         // instantiate a contract
         let mut contract = ProjectManagement::new();
         for i in 1..5 {
-            contract.add_project(i.to_string(),
+            let result = contract.add_project(i.to_string(),
                                  "https://github.com/test-project/issues/1".to_string(),
                                  "This is a test".to_string(),
                                  "2000".parse().unwrap());
+            assert_eq!(result.is_ok(), true);
             let projects = contract.get_all_projects();
             assert_eq!(projects.not_started.len(), i as usize);
             assert_eq!(projects.in_progress.len(), 0 as usize);
             assert_eq!(projects.complete.len(), 0 as usize);
         }
         for i in 1..3 {
-            contract.set_user_for_project(i.to_string(), context.clone().current_account_id);
+            let result = contract.set_user_for_project(i.to_string(), context.clone().current_account_id);
+            assert_eq!(result.is_ok(), true);
             let projects = contract.get_all_projects();
             assert_eq!(projects.not_started.len(), 4 - i as usize);
             assert_eq!(projects.in_progress.len(), i as usize);
@@ -71,21 +73,23 @@ mod tests {
     #[test]
     fn test_get_user_projects() {
         // set up the mock context into the testing environment
-        let context = get_context(vec![], false);
+        let context = get_context(vec![], false, "alice.testnet".to_string());
         testing_env!(context.clone());
         // instantiate a contract
         let mut contract = ProjectManagement::new();
         for i in 1..5 {
-            contract.add_project(i.to_string(),
+            let result = contract.add_project(i.to_string(),
                                  "https://github.com/test-project/issues/1".to_string(),
                                  "This is a test".to_string(),
                                  "2000".parse().unwrap());
+            assert_eq!(result.is_ok(), true);
             let projects = contract.get_user_projects(context.clone().current_account_id);
             assert_eq!(projects.in_progress.len(), 0 as usize);
             assert_eq!(projects.complete.len(), 0 as usize);
         }
         for i in 1..3 {
-            contract.set_user_for_project(i.to_string(), context.clone().current_account_id);
+            let result = contract.set_user_for_project(i.to_string(), context.clone().current_account_id);
+            assert_eq!(result.is_ok(), true);
             let projects = contract.get_user_projects(context.clone().current_account_id);
             assert_eq!(projects.in_progress.len(), i as usize);
             assert_eq!(projects.complete.len(), 0 as usize);
@@ -102,21 +106,22 @@ mod tests {
     // This test should insert a new project with no errors
     fn test_create_project() {
         // set up the mock context into the testing environment
-        let context = get_context(vec![], false);
+        let context = get_context(vec![], false, "alice.testnet".to_string());
         testing_env!(context.clone());
         // instantiate a contract
         let mut contract = ProjectManagement::new();
-        contract.add_project("1".parse().unwrap(),
+        let result = contract.add_project("1".parse().unwrap(),
                                       "https://github.com/test-project/issues/1".to_string(),
                                       "This is a test".to_string(),
                                       "2000".parse().unwrap());
+        assert_eq!(result.is_ok(), true);
     }
 
     #[test]
     // This test should add a user to a project with no errors
     fn test_add_user_to_project() {
         // set up the mock context into the testing environment
-        let context = get_context(vec![], false);
+        let context = get_context(vec![], false, "alice.testnet".to_string());
         testing_env!(context.clone());
         // instantiate a contract
         let worker_account = AccountId::from("subaccount.example.near");
@@ -125,13 +130,17 @@ mod tests {
                                       "https://github.com/test-project/issues/1".to_string(),
                                       "This is a test".to_string(),
                                       "100".parse().unwrap());
-        contract.set_user_for_project(id.clone(), worker_account.clone());
+        assert_eq!(id.is_ok(), true);
+        let result = contract.set_user_for_project(id.clone().unwrap(), worker_account.clone());
+        assert_eq!(result.is_ok(), true);
         // This second part test that it can add to a existing set
         let id = contract.add_project("2".parse().unwrap(),
                                       "https://github.com/test-project/issues/2".to_string(),
                                       "This is a test 2".to_string(),
                                       "2000".parse().unwrap());
-        contract.set_user_for_project(id.clone(), worker_account.clone());
-        contract.set_project_complete(id.clone());
+        assert_eq!(id.is_ok(), true);
+        let result = contract.set_user_for_project(id.clone().unwrap(), worker_account.clone());
+        assert_eq!(result.is_ok(), true);
+        contract.set_project_complete(id.clone().unwrap());
     }
 }
--- src/structs/project_management.rs
+++ src/structs/project_management.rs
@@ -1,5 +1,5 @@
 use near_sdk::collections::{LookupMap, UnorderedSet};
-use near_sdk::{AccountId, Promise};
+use near_sdk::{AccountId, Promise, env};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{near_bindgen};
 use near_sdk::PanicOnDefault;
@@ -10,9 +10,10 @@ use crate::structs::user_project_returns::UserProjectsReturn;
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct ProjectManagement {
+    // The reason for user_projects and user_ids is to reduce the loop time when getting/removing user_projects
     pub(crate) project_ids: UnorderedSet<String>,
     pub(crate) projects: LookupMap<String, Project>,
-    // The reason for user_projects and user_ids is to reduce the loop time when getting user_projects
+    // The reason for user_projects and user_ids is to reduce the loop time when getting/removing user_projects
     pub(crate) user_ids: UnorderedSet<String>,
     pub(crate) user_projects: LookupMap<AccountId, LookupMap<String, Project>>
 }
@@ -35,67 +36,81 @@ impl ProjectManagement {
         github_issue_link: String,
         description: String,
         reward: String
-    ) -> String {
-        self.projects.insert(&id, &Project::new(
-            id.clone(),
-            github_issue_link,
-            description,
-            reward.parse().unwrap()
-        ));
-        self.project_ids.insert(&id);
-        id
+    ) -> Result<String, String> {
+        if env::current_account_id() != env::signer_account_id() {
+            Err("Only the company can create a project.".to_string())
+        } else {
+            self.projects.insert(&id, &Project::new(
+                id.clone(),
+                github_issue_link,
+                description,
+                reward.parse().unwrap()
+            ));
+            self.project_ids.insert(&id);
+            Ok(id)
+        }
     }
 
     pub fn update_project(
-        mut self,
+        &mut self,
         id: String,
         github_issue_link: Option<String>,
         description: Option<String>,
         reward: Option<String>
-    ) {
-        let mut project = self.projects.get(&id).unwrap();
-        match github_issue_link {
-            None => {}
-            Some(link) => {
-                project.github_issue_link = link;
+    ) -> Result<String, String> {
+        if env::current_account_id() != env::signer_account_id() {
+            Err("Only the company can create a project.".to_string())
+        } else {
+            let mut project = self.projects.get(&id).unwrap();
+            match github_issue_link {
+                None => (),
+                Some(link) => {
+                    project.github_issue_link = link;
+                }
             }
-        }
-        match description {
-            None => {}
-            Some(desc) => {
-                project.description = desc;
+            match description {
+                None => (),
+                Some(desc) => {
+                    project.description = desc;
+                }
             }
-        }
-        match reward {
-            None => {}
-            Some(reward_string) => {
-                project.reward = reward_string.parse().unwrap();
+            match reward {
+                None => (),
+                Some(reward_string) => {
+                    project.reward = reward_string.parse().unwrap();
+                }
             }
+            self.projects.remove(&id);
+            self.projects.insert(&id, &project);
+            Ok(id)
         }
-        self.projects.remove(&id);
-        self.projects.insert(&id, &project);
     }
 
-    pub fn set_user_for_project(&mut self, id: String, worker_id: String) {
+    pub fn set_user_for_project(&mut self, id: String, worker_id: String) -> Result<(), String> {
         let mut project = self.projects.get(&id).unwrap();
-        project.worker = Option::from(worker_id.clone());
-        project.status = Status::InProgress;
-        self.projects.remove(&id);
-        self.projects.insert(&id, &project);
-        let user_projects_result = self.user_projects.get(&worker_id);
-        let user_projects = match user_projects_result {
-            None => {
-                let mut set: LookupMap<String, Project> = LookupMap::new(b"p");
-                set.insert(&id, &self.projects.get(&id).unwrap());
-                set
-            }
-            Some(mut set) => {
-                set.insert(&id, &self.projects.get(&id).unwrap());
-                set
-            }
-        };
-        self.user_ids.insert(&id);
-        self.user_projects.insert(&worker_id, &user_projects);
+        if project.worker.is_some() {
+            Err("The project done has a user attached to it. Please remove the user before preceding.".to_string())
+        } else {
+            project.worker = Option::from(worker_id.clone());
+            project.status = Status::InProgress;
+            self.projects.remove(&id);
+            self.projects.insert(&id, &project);
+            let user_projects_result = self.user_projects.get(&worker_id);
+            let user_projects = match user_projects_result {
+                None => {
+                    let mut set: LookupMap<String, Project> = LookupMap::new(b"p");
+                    set.insert(&id, &self.projects.get(&id).unwrap());
+                    set
+                }
+                Some(mut set) => {
+                    set.insert(&id, &self.projects.get(&id).unwrap());
+                    set
+                }
+            };
+            self.user_ids.insert(&id);
+            self.user_projects.insert(&worker_id, &user_projects);
+            Ok(())
+        }
     }
 
     pub fn set_project_complete(&mut self, id: String) -> Promise {
@@ -149,5 +164,53 @@ impl ProjectManagement {
             }
         }
         projects
+    }
+
+    pub fn remove_user_from_project(&mut self, job_id: String, worker_id: String) -> Result<(), String> {
+        let mut project = self.projects.get(&job_id).unwrap();
+        match project.status {
+            Status::Complete => {
+                Err("The job is marked as complete and therefore you cannot remove user from job.".to_string())
+            }
+            Status::InProgress => {
+                // check if the worker or the the company requested it
+                if project.worker.unwrap() == env::signer_account_id() || env::current_account_id() == env::signer_account_id() {
+                    project.worker = None;
+                    project.status = Status::Created;
+                    self.user_projects.get(&worker_id).unwrap().remove(&job_id).unwrap();
+                    self.user_ids.remove(&job_id);
+                    self.projects.remove(&job_id).unwrap();
+                    self.projects.insert(&job_id, &project).unwrap();
+                    Ok(())
+                } else {
+                    Err("The job owner or the company must request it to be removed.".to_string())
+                }
+            }
+            Status::Created => {
+                Err("The job has no user assigned to it.".to_string())
+            }
+        }
+    }
+
+    pub fn remove_project(&mut self, job_id: String) -> Result<(), String> {
+        let project = self.projects.get(&job_id).unwrap();
+        match project.status {
+            Status::Complete => {
+                Err("The job is complete and therefore cannot be removed.".to_string())
+            }
+            Status::InProgress => {
+                Err("The job is in progress. You must first remove the user from job.".to_string())
+            }
+            Status::Created => {
+                // check if the company requested it
+                if env::current_account_id() == env::signer_account_id() {
+                    self.projects.remove(&job_id).unwrap();
+                    self.project_ids.remove(&job_id);
+                    Ok(())
+                } else {
+                    Err("The company must request this.".to_string())
+                }
+            }
+        }
     }
 }
