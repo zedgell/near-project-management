e9e97ec62a1c137c84bf13e55e266bfb86da5e7b
Zachary K Edgell <zachary@accrue.tech>
2021-12-26 05:56:50 -0500

    disabled a couple shell checks due to false positive.

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 JSON_FILE=$(find ~/.near-credentials/testnet/ -regextype posix-extended -regex ".*/[A-Za-z0-9_,\\s]+\.testnet\.json")
+# shellcheck disable=SC2002
 ACCOUNT_ID=$(cat "$JSON_FILE" | jq -r ".account_id")
 
 near delete near_project_management."$ACCOUNT_ID" "$ACCOUNT_ID"
