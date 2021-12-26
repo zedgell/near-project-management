bbbd29a15de9d0533a5c8b10e2ec7058b9f34afb
Zachary K Edgell <zachary@accrue.tech>
2021-12-26 05:55:58 -0500

    Added scripts to package.json to build, run unit test, run integration test, deploy, and cleanup. I also added bash scripts for deploy and cleanup.

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+JSON_FILE=$(find ~/.near-credentials/testnet/ -regextype posix-extended -regex ".*/[A-Za-z0-9_,\\s]+\.testnet\.json")
+ACCOUNT_ID=$(cat "$JSON_FILE" | jq -r ".account_id")
+
+near delete near_project_management."$ACCOUNT_ID" "$ACCOUNT_ID"
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JSON_FILE=$(find ~/.near-credentials/testnet/ -regextype posix-extended -regex ".*/[A-Za-z0-9_,\\s]+\.testnet\.json")
+ACCOUNT_ID=$(cat "$JSON_FILE" | jq -r ".account_id")
+
+near create-account near_project_management."$ACCOUNT_ID" --masterAccount "$ACCOUNT_ID"
+
+near deploy near_project_management."$ACCOUNT_ID" ./target/wasm32-unknown-unknown/release/near_project_management.wasm
+
+near call near_project_management."$ACCOUNT_ID" new --accountId near_project_management."$ACCOUNT_ID"
